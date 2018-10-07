8b818011ab5deb3a51478f3469e824106cd84348
A.Rahman <a.rahman.asad22@gmail.com>
2018-10-07 14:38:12 +0200

    Initial Commit

---
+++ programmingLanguages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  programmingLanguages
+//
+//  Created by A.Rahman on 10/7/18.
+//  Copyright © 2018 per. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
